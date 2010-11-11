c9534f50bf761130571a3caa57a42dddc6425866
haowaiwai <haowaiwai@ymail.com>
2010-11-11 13:48:58 +0800

    create

---
+++ helloworld/src/haoyy/android/hello.java
@@ -0,0 +1,14 @@
+package haoyy.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class hello extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	//	ok
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
