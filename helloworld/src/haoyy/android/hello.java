29e1fb27f60b086d2de84b0bd3266d1ad404185e
haowaiwai <haowaiwai@ymail.com>
2010-11-11 13:54:50 +0800

    添加注释信息

--- helloworld/src/haoyy/android/hello.java
+++ helloworld/src/haoyy/android/hello.java
@@ -7,7 +7,7 @@ public class hello extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	//	ok
+    	//	向导生成
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
